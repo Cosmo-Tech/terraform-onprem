33ad2067ceafd5993cf8b611733244506b951980
ggtrd <103002419+ggtrd@users.noreply.github.com>
2026-03-26 16:39:36 +0100

    fix nodes labels

--- terraform-cluster/modules/nodes_labels/main.tf
+++ terraform-cluster/modules/nodes_labels/main.tf
@@ -1,9 +1,6 @@
 locals {
   node_ip_to_name = {
-    for node in data.kubernetes_nodes.all_nodes.nodes :
-    flatten([
-      for addr in node.status[0].addresses : addr.address if addr.type == "InternalIP"
-    ])[0] => node.metadata[0].name
+    for node in data.kubernetes_nodes.all_nodes.nodes : flatten([for addr in node.status[0].addresses : addr.address if addr.type == "InternalIP"])[0] => node.metadata[0].name
   }
 }
 
@@ -17,9 +14,9 @@ resource "terraform_data" "trigger_refresh" {
 }
 
 
-# Set cosmotech.com/tier
+# Set cosmotech labels
 # The labels are automatically applied on nodes based on their real IP address vs the IP address you setted in terraform.tfvars
-resource "kubernetes_labels" "cosmotech_tiers" {
+resource "kubernetes_labels" "cosmotech_labels" {
   for_each = {
     for key, value in var.hosts : key => value
     if value.type != "controlplane" && contains(keys(local.node_ip_to_name), value.ip)
@@ -36,33 +33,8 @@ resource "kubernetes_labels" "cosmotech_tiers" {
 
   labels = {
     "cosmotech.com/tier" = each.value.type
-  }
-
-  lifecycle {
-    replace_triggered_by = [terraform_data.trigger_refresh]
-  }
-}
-
-
-# Set cosmotech.com/size (on cosmotech.com/tier=compute)
-# The labels are automatically applied on nodes based on their real IP address vs the IP address you setted in terraform.tfvars
-resource "kubernetes_labels" "cosmotech_tier_compute_sizing" {
-  for_each = {
-    for key, value in var.hosts : key => value
-    if value.type == "compute" && contains(keys(local.node_ip_to_name), value.ip)
-  }
-
-  api_version   = "v1"
-  kind          = "Node"
-  force         = true
-  field_manager = "terraform-cosmotech"
-
-  metadata {
-    name = local.node_ip_to_name[each.value.ip]
-  }
-
-  labels = {
-    "cosmotech.com/size" = each.value.size
+    "cosmotech.com/size" = each.value.type == "compute" ? each.value.size : null
+    "vendor" = "cosmotech"
   }
 
   lifecycle {
@@ -95,14 +67,4 @@ resource "kubernetes_node_taint" "cosmotech_taints" {
   lifecycle {
     replace_triggered_by = [terraform_data.trigger_refresh]
   }
-}
-
-
-# The taints & labels can take time to be applied. Just a timer to be sure the nexts Terraform resources will works as expected
-resource "time_sleep" "wait_for_kubernetes_propagation" {
-  create_duration = "30s"
-  depends_on = [
-    kubernetes_labels.cosmotech_tiers,
-    kubernetes_node_taint.cosmotech_taints
-  ]
 }
